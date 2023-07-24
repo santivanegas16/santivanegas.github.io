da9c20eaa1b480b715228b8646a0e3ebd680e40a
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-07-23 23:42:42 -0500

    fixed projects

--- src/pages/Proyect.jsx
+++ src/pages/Proyect.jsx
@@ -15,7 +15,7 @@ export default function Proyect() {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </a></h1>
-              <p className='text-justify'>Desarrollo de una página web dinámica y responsiva de cómics y manga utilizando MongoDB, Express.js, React.js, Node.js, Tailwind CSS y metodología SCRUM. Proporcionando a los usuarios una amplia variedad de contenido para explorar y disfrutar.</p>
+              <p className='text-justify'>Development of a dynamic and responsive comic and manga website using MongoDB, Express.js, React.js, Node.js, Tailwind CSS and SCRUM methodology. Providing users with a wide variety of content to explore and enjoy.</p>
             </div>
           </div>
 
@@ -27,10 +27,7 @@ export default function Proyect() {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </a></h1>
-              <p className='text-justify'>Desarrollo de una página web dinámica y responsiva para una pet shop. Utilizando HTML,
-                CSS, JavaScript, Bootstrap e integrando el consumo de una API; que permite postear un
-                nuevo producto para que se visualice automáticamente en el catálogo, además, un carrito
-                de compras funcional, donde se pueden agregar dichos productos y comprarlos.</p>
+              <p className='text-justify'>Development of a dynamic and responsive website for a pet shop. Using HTML, CSS, JavaScript, Bootstrap and integrating the use of an API; which allows posting a new product to be automatically displayed in the catalog, also, a functional shopping cart, where you can add the products and buy them.</p>
             </div>
           </div>
 
@@ -42,10 +39,7 @@ export default function Proyect() {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </a></h1>
-              <p className='text-justify'>Desarrollo de una página web para una empresa de eventos. Haciendo uso de HTML, CSS,
-                JavaScript, SwiperJS y Bootstrap. Integrando el consumo de una API, para obtener los
-                datos sobre los eventos; permitiendo visualizar de manera óptima los eventos presentados y
-                filtrados.</p>
+              <p className='text-justify'>Development of a website for an events company. Using HTML, CSS, JavaScript, SwiperJS and Bootstrap. Integrating the consumption of an API, to obtain data about the events; allowing to visualize in an optimal way the events presented and filtered.</p>
             </div>
           </div>
         </div>
